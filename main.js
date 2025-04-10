89a34474e46b8c86b81becefded43045c3b4a35c
HighNoonKieran <k2bunzai@gmail.com>
2025-04-09 20:58:28 -0600

    added a hero image
    added hero image as well as a few images on the guest page

--- main.js
+++ main.js
@@ -3,19 +3,17 @@ const d = new Date();
 fYear.textContent = d.getFullYear();
 
 fetch(
-	'https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=625a907c29f418b5627de1371aef0f30&units=metric'
+'https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=625a907c29f418b5627de1371aef0f30&units=metric'
 )
 .then(response => response.json())
 .then(data => {
 	console.log(data);
     console.log(data.main.temp);
-    temperature.textContent = "Temperature: "+data.main.temp+"\u00B0C";
-    feels.textContent = "Feels Like: "+data.main.feels_like+"\u00B0C";;
+    temperature.textContent = "Temperature in Cancun: "+data.main.temp+"\u00B0C";
 });
 
 const navToggle = document.getElementById('nav-toggle'); 
 const menuList = document.getElementById('main-menu'); 
-
 navToggle.addEventListener('click', function(){
     menuList.classList.toggle('active');
 });
